9904bc154c5a3d17f99dcff442e3a2a07bf5cafa
Gabrielcpellanda <gabrielpellanda@gmail.com>
2023-05-26 15:34:35 -0300

    Finalizada v1

---
+++ MoneyExtension.Tests/MoneyExtensionTests.cs
@@ -0,0 +1,16 @@
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MoneyExtension.Tests;
+
+[TestClass]
+public class MoneyExtensionTests
+{
+    [TestMethod]
+    public void ShouldConvertDecimalToInt()
+    {
+        decimal valor = 279.98M;
+        var cents = valor.ToCents();
+        
+        Assert.AreEqual(27998, cents);
+    }
+}
